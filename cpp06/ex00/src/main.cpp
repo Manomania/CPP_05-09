cdea33d4b4a1a4486e27fecdc803580900a7b511
maximart <maxime.martinez96@hotmail.fr>
2025-10-15 08:11:30 +0200

    feat(error): handle a message error if no parameters

--- cpp06/ex00/src/main.cpp
+++ cpp06/ex00/src/main.cpp
@@ -1,9 +1,11 @@
+#include <caca_conio.h>
 #include "ScalarConverter.hpp"
 
 int main(int argc, char **argv) {
 	if (argc == 2) {
 		ScalarConverter::convert(argv[1]);
 	} else {
+		std::cout << "\x1B[31mUsage: " << argv[0] << " <parameter1>\033[0m\t\t"<< std::endl;
 		return (1);
 	}
 	return (0);
