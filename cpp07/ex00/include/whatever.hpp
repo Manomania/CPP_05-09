b4e8a9b4af91c564528898e197ebc0ffe36c861b
maximart <maxime.martinez96@hotmail.fr>
2025-10-16 10:02:48 +0200

    feat: add the necessary files, done

--- cpp07/ex00/include/whatever.hpp
+++ cpp07/ex00/include/whatever.hpp
@@ -1,12 +1,21 @@
 #ifndef WHATEVER_HPP
 #define WHATEVER_HPP
 
-#include <iomanip>
-
 template<typename T>
-T swap(T a, T b){
-	return (a > b) ? a : b;
+void swap(T &a, T &b){
+	T temp = a;
+	a = b;
+	b = temp;
 }
 
+template<typename U>
+U min(U a, U b) {
+	return (a < b) ? a : b;
+}
+
+template<typename V>
+V max (V a, V b) {
+	return (a > b) ? a : b;
+}
 
 #endif
--- cpp07/ex00/src/main.cpp
+++ cpp07/ex00/src/main.cpp
@@ -1,8 +1,13 @@
 #include <iostream>
+#include "whatever.hpp"
 
 int main( ) {
 	int a = 2;
 	int b = 3;
+	::swap( a, b );
+	std::cout << "a = " << a << ", b = " << b << std::endl;
+	::swap( a, b );
+	std::cout << "a = " << a << ", b = " << b << std::endl;
 	::swap( a, b );
 	std::cout << "a = " << a << ", b = " << b << std::endl;
 	std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
