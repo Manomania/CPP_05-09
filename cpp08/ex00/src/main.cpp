0331fce6deabc36b064b6097964505a4030fc8aa
maximart <maxime.martinez96@hotmail.fr>
2025-10-27 17:00:41 +0100

    feat(main): enhance the lisibility and add some tests

--- cpp08/ex00/src/main.cpp
+++ cpp08/ex00/src/main.cpp
@@ -3,17 +3,32 @@
 #include <vector>
 #include <list>
 
-int main() {
-	std::vector<int> vector;
-	std::list<int> list;
-	for (int i = 0; i < 10; i++) {
-		vector.push_back(i);
-		list.push_back(i+3);
+template<typename T>
+void	testContainer(T& container, const int n) {
+	for (int i = 0; i < n; i++) {
+		if (i % 2 == 0)
+			container.push_back(i);
 	}
-	try {
-		easyfind(vector, 2000);
-	} catch (const std::exception& e) {
-		std::cout << e.what() << std::endl;
+}
+
+int main(int argc, char **argv) {
+	if (argc != 2)
+		return (1);
+	else {
+		try {
+			std::vector<int> vector;
+			testContainer(vector, 10);
+			easyfind(vector, std::atoi(argv[1]));
+		} catch (const std::exception& e) {
+			std::cout << e.what() << std::endl;
+		}
+		try {
+			std::list<int> list;
+			testContainer(list, 10);
+			easyfind(list, std::atoi(argv[1]));
+		} catch (const std::exception& e) {
+			std::cout << e.what() << std::endl;
+		}
 	}
 	return (0);
 }
