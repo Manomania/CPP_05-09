aa2c227e47bfed188676d56f278df0918060bb72
maximart <maxime.martinez96@hotmail.fr>
2025-11-20 18:59:55 +0100

    feat: add some todo and basics

--- cpp09/ex02/include/PmergeMe.hpp
+++ cpp09/ex02/include/PmergeMe.hpp
@@ -2,7 +2,13 @@
 #define PMERGEME_HPP
 
 class PmergeMe {
-
+private:
+	//TODO use vector and (queue or pair)
+public:
+	PmergeMe();
+	PmergeMe(const PmergeMe& copy);
+	PmergeMe& operator=(const PmergeMe& other);
+	~PmergeMe();
 };
 
 #endif
--- cpp09/ex02/src/PmergeMe.cpp
+++ cpp09/ex02/src/PmergeMe.cpp
@@ -1,2 +1,15 @@
 #include "PmergeMe.hpp"
 
+PmergeMe::PmergeMe() {}
+
+PmergeMe::PmergeMe(const PmergeMe& copy) {
+	//TODO copy constructor
+}
+
+PmergeMe& PmergeMe::operator=(const PmergeMe& other) {
+	if (this != &other)
+		//TODO overload operator =
+	return (*this);
+}
+
+PmergeMe::~PmergeMe() {}
