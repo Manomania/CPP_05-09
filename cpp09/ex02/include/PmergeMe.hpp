eca194f8cd3a0d7439928810f105f7a26c520894
manomania <maxime.martinez96@hotmail.fr>
2025-11-26 11:28:00 +0100

    feat(template): use template to enhance my function for deque or vector, before it was only for vector. change index research to iterator for deque principally.

--- cpp09/ex02/include/PmergeMe.hpp
+++ cpp09/ex02/include/PmergeMe.hpp
@@ -4,6 +4,8 @@
 #include <vector>
 #include <deque>
 #include <iostream>
+#include <iterator>
+#include <algorithm>
 
 class PmergeMe {
 private:
@@ -16,12 +18,134 @@ public:
 	~PmergeMe();
 	void	setVector(const std::vector<int>& vector);
 	void	setDeque(const std::deque<int>& deque);
-	void				swapPairs(std::vector<int>& vec, size_t leftStart, size_t rightStart, size_t size);
-	std::vector<int>&	createPairVec(std::vector<int>& vec, size_t& pairSize, size_t& vecSize);
 	std::vector<size_t> generateJacobsthal(size_t n);
 	size_t				getJacobsthalNumber(size_t index);
-	void				insertWithJacobsthal(std::vector<int>& vec, std::vector<int>& pend, size_t& pairSize, size_t& incompletePair);
-	void				dichotomicSearch(std::vector<int>& vec, std::vector<int>& stragglers);
-	void				insertPendingElements(std::vector<int>& vec, size_t pairSize, size_t vecSize);
+
+	template<typename Container>
+	void swapPairs(Container& container, typename Container::iterator leftStart, typename Container::iterator rightStart, size_t size);
+
+	template<typename Container>
+	void insertWithJacobsthal(Container& container, Container& pend, size_t pairSize, size_t incompletePair);
+
+	template<typename Container>
+	void dichotomicSearch(Container& container, Container& stragglers);
+
+	template<typename Container>
+	void insertPendingElements(Container& container, size_t pairSize, size_t containerSize);
+
+	template<typename Container>
+	Container& createPairCont(Container& container, size_t& pairSize, size_t& containerSize);
 };
+
+template<typename Container>
+void PmergeMe::swapPairs(Container& container, typename Container::iterator leftStart, typename Container::iterator rightStart, size_t size) {
+	(void)container;
+	typename Container::iterator left = leftStart;
+	typename Container::iterator right = rightStart;
+	for (size_t i = 0; i < size; i++) {
+		std::iter_swap(left, right);
+		++left;
+		++right;
+	}
+}
+
+template<typename Container>
+void PmergeMe::insertWithJacobsthal(Container& container, Container& pend, size_t pairSize, size_t incompletePair) {
+	std::vector<size_t> jacobSeq = generateJacobsthal(incompletePair);
+	for (size_t i = 0; i < jacobSeq.size(); i++) {
+		size_t groupIndex = jacobSeq[i] - 1;
+		typename Container::iterator pendStart = pend.begin();
+		std::advance(pendStart, groupIndex * pairSize);
+		typename Container::iterator pendEnd = pendStart;
+		std::advance(pendEnd, pairSize);
+		Container insertInContainer(pendStart, pendEnd);
+		for (typename Container::iterator it = insertInContainer.begin(); it != insertInContainer.end(); ++it) {
+			typename Container::iterator left = container.begin();
+			typename Container::iterator right = container.end();
+			while (std::distance(left, right) > 0) {
+				typename Container::iterator mid = left;
+				std::advance(mid, std::distance(left, right) / 2);
+				if (*mid > *it) {
+					right = mid;
+				} else {
+					left = mid;
+					++left;
+				}
+			}
+			container.insert(left, *it);
+		}
+	}
+}
+
+template<typename Container>
+void PmergeMe::dichotomicSearch(Container& container, Container& stragglers) {
+	for (typename Container::iterator straggler = stragglers.begin(); straggler != stragglers.end(); ++straggler) {
+		typename Container::iterator left = container.begin();
+		typename Container::iterator right = container.end();
+		while (std::distance(left, right) > 0) {
+			typename Container::iterator mid = left;
+			std::advance(mid, std::distance(left, right) / 2);
+			if (*mid > *straggler) {
+				right = mid;
+			} else {
+				left = mid;
+				++left;
+			}
+		}
+		container.insert(left, *straggler);
+	}
+}
+
+template<typename Container>
+void PmergeMe::insertPendingElements(Container& container, size_t pairSize, size_t containerSize) {
+	size_t completPair = containerSize / (pairSize * 2);
+	size_t mainSize = completPair * pairSize * 2;
+	size_t waiting = containerSize - mainSize;
+	size_t incompletePair = waiting / pairSize;
+	size_t pendSize = incompletePair * pairSize;
+	size_t garbage = waiting % pairSize;
+	Container pend;
+	Container stragglers;
+	typename Container::iterator mainEnd = container.begin();
+	std::advance(mainEnd, mainSize);
+	if (pendSize > 0) {
+		typename Container::iterator pendEnd = mainEnd;
+		std::advance(pendEnd, pendSize);
+		pend.assign(mainEnd, pendEnd);
+	}
+	if (garbage > 0) {
+		typename Container::iterator pendEnd = mainEnd;
+		std::advance(pendEnd, pendSize);
+		stragglers.assign(pendEnd, container.end());
+	}
+	container.erase(mainEnd, container.end());
+	if (incompletePair > 0)
+		insertWithJacobsthal(container, pend, pairSize, incompletePair);
+	dichotomicSearch(container, stragglers);
+}
+
+template<typename Container>
+Container& PmergeMe::createPairCont(Container& container, size_t& pairSize, size_t& containerSize) {
+	pairSize *= 2;
+	if (pairSize > containerSize)
+		return container;
+	for (size_t i = pairSize - 1; i < container.size(); i += pairSize) {
+		typename Container::iterator leftPairMax = container.begin();
+		std::advance(leftPairMax, i - pairSize / 2);
+		typename Container::iterator rightPairMax = container.begin();
+		std::advance(rightPairMax, i);
+		if (*leftPairMax > *rightPairMax) {
+			typename Container::iterator leftStart = container.begin();
+			std::advance(leftStart, i - pairSize + 1);
+			typename Container::iterator rightStart = container.begin();
+			std::advance(rightStart, i - pairSize / 2 + 1);
+			swapPairs(container, leftStart, rightStart, pairSize / 2);
+		}
+	}
+	createPairCont(container, pairSize, containerSize);
+	pairSize /= 2;
+	insertPendingElements(container, pairSize, containerSize);
+	return container;
+}
+
 #endif
--- cpp09/ex02/src/PmergeMe.cpp
+++ cpp09/ex02/src/PmergeMe.cpp
@@ -1,16 +1,14 @@
 #include "PmergeMe.hpp"
-#include <sstream>
-
 
 void	PmergeMe::setVector(const std::vector<int>& vector) {
 	_vector = vector;
 }
+
 void	PmergeMe::setDeque(const std::deque<int>& deque) {
 	_deque = deque;
 }
 
 PmergeMe::PmergeMe(): _vector(), _deque() {}
-
 
 PmergeMe::PmergeMe(const PmergeMe& copy): _vector(copy._vector), _deque(copy._deque) {}
 
@@ -23,12 +21,6 @@ PmergeMe& PmergeMe::operator=(const PmergeMe& other) {
 }
 
 PmergeMe::~PmergeMe() {}
-
-void PmergeMe::swapPairs(std::vector<int>& vec, size_t leftStart, size_t rightStart, size_t size) {
-	for (size_t i = 0; i < size; i++) {
-		std::swap(vec[leftStart + i], vec[rightStart + i]);
-	}
-}
 
 size_t PmergeMe::getJacobsthalNumber(size_t index) {
 	if (index == 0)
@@ -65,74 +57,4 @@ std::vector<size_t> PmergeMe::generateJacobsthal(size_t n) {
 		jacobIndex++;
 	}
 	return sequence;
-}
-
-void PmergeMe::insertWithJacobsthal(std::vector<int>& vec, std::vector<int>& pend, size_t& pairSize, size_t& incompletePair) {
-	std::vector<size_t> jacobSeq = generateJacobsthal(incompletePair);
-	for (size_t i = 0; i < jacobSeq.size(); i++) {
-		size_t groupIndex = jacobSeq[i] - 1;
-		std::vector<int> insertInVec(pend.begin() + groupIndex * pairSize, pend.begin() + (groupIndex + 1) * pairSize);
-		for (size_t j = 0; j < insertInVec.size(); j++) {
-			size_t left = 0;
-			size_t right = vec.size();
-			while (left < right) {
-				size_t mid = left + (right - left) / 2;
-				if (vec[mid] > insertInVec[j])
-					right = mid;
-				else
-					left = mid + 1;
-			}
-			vec.insert(vec.begin() + left, insertInVec[j]);
-		}
-	}
-}
-
-void PmergeMe::dichotomicSearch(std::vector<int>& vec, std::vector<int>& stragglers) {
-	for (size_t i = 0; i < stragglers.size(); i++) {
-		size_t left = 0;
-		size_t right = vec.size();
-		while (left < right) {
-			size_t mid = left + (right - left) / 2;
-			if (vec[mid] > stragglers[i])
-				right = mid;
-			else
-				left = mid + 1;
-		}
-		vec.insert(vec.begin() + left, stragglers[i]);
-	}
-}
-
-void PmergeMe::insertPendingElements(std::vector<int>& vec, size_t pairSize, size_t vecSize) {
-	size_t completPair = vecSize / (pairSize * 2);
-	size_t mainSize = completPair * pairSize * 2;
-	size_t waiting = vecSize - mainSize;
-	size_t incompletePair = waiting / pairSize;
-	size_t pendSize = incompletePair * pairSize;
-	size_t garbage = waiting % pairSize;
-	std::vector<int> pend;
-	std::vector<int> stragglers;
-	if (pendSize > 0)
-		pend.assign(vec.begin() + mainSize, vec.begin() + mainSize + pendSize);
-	if (garbage > 0)
-		stragglers.assign(vec.begin() + mainSize + pendSize, vec.begin() + vecSize);
-	vec.erase(vec.begin() + mainSize, vec.end());
-	if (incompletePair > 0)
-		insertWithJacobsthal(vec, pend, pairSize, incompletePair);
-	dichotomicSearch(vec, stragglers);
-}
-
-std::vector<int>&	PmergeMe::createPairVec(std::vector<int>& vec, size_t& pairSize, size_t& vecSize) {
-	pairSize *= 2;
-	if (pairSize > vecSize)
-		return vec;
-	for (size_t i = pairSize - 1; i < vec.size(); i += pairSize) {
-		size_t leftPairMax = i - pairSize / 2;
-		size_t rightPairMax = i;
-		if (vec[leftPairMax] > vec[rightPairMax])
-			swapPairs(vec, (i - pairSize + 1), (i - pairSize / 2 + 1), (pairSize / 2));
-	}
-	createPairVec(vec, pairSize, vecSize);
-	pairSize /= 2;
-	insertPendingElements(vec, pairSize, vecSize);
-	return vec;
 }
