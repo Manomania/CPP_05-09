ff9b191f164c7fb561c1c043a5585132c909d7bf
maximart <maxime.martinez96@hotmail.fr>
2025-11-28 17:51:19 +0100

    chore: add more test and colors

--- cpp08/ex01/src/main.cpp
+++ cpp08/ex01/src/main.cpp
@@ -24,8 +24,9 @@ void displayVector(const T& container) {
 }
 
 int main() {
-	Span sp = Span(100);
+	std::cout << RED REVERSE"\nTEST WITH ADD RANDOM NUMBER"RESET << std::endl;
 	std::cout << RED ULINE"\nTEST WITH 3"RESET << std::endl;
+	Span sp = Span(100);
 	try {
 		sp.addRandomNumbers(3);
 		std::cout << "Inside Span: "YELLOW;
@@ -79,6 +80,101 @@ int main() {
 		std::cout << RESET;
 		std::cout << "Shortest length: "BLUE<< s3.shortestSpan() << RESET << std::endl;
 		std::cout << "Longest length: "MAGENTA << s3.longestSpan() << RESET << std::endl;
+	} catch (std::exception& e) {
+		std::cout << "Error: " << e.what() << RESET << std::endl;
+	}
+
+	std::cout << RED REVERSE"\nTEST WITH ADD NUMBER"RESET << std::endl;
+	std::cout << RED ULINE"\nTEST WITH 3"RESET << std::endl;
+	Span s4 = Span(100);
+	try {
+		s4.addNumber(1);
+		s4.addNumber(2);
+		s4.addNumber(3);
+		std::cout << "Inside Span: "YELLOW;
+		displayVector(s4);
+		std::cout << RESET;
+		std::cout << "Shortest length: "BLUE<< s4.shortestSpan() << RESET << std::endl;
+		std::cout << "Longest length: "MAGENTA << s4.longestSpan() << RESET << std::endl;
+	} catch (std::exception& e) {
+		std::cout << "Error: " << e.what() << RESET << std::endl;
+	}
+	std::cout << RED ULINE"\nTEST WITH 10 MORE"RESET << std::endl;
+	try {
+		s4.addNumber(4);
+		s4.addNumber(5);
+		s4.addNumber(6);
+		s4.addNumber(7);
+		s4.addNumber(8);
+		s4.addNumber(9);
+		s4.addNumber(10);
+		s4.addNumber(11);
+		s4.addNumber(12);
+		s4.addNumber(13);
+		std::cout << "Inside Span: "YELLOW;
+		displayVector(s4);
+		std::cout << RESET;
+		std::cout << "Shortest length: "BLUE<< s4.shortestSpan() << RESET << std::endl;
+		std::cout << "Longest length: "MAGENTA << s4.longestSpan() << RESET << std::endl;
+	} catch (std::exception& e) {
+		std::cout << "Error: " << e.what() << RESET << std::endl;
+	}
+	Span s5 = Span(10);
+	std::cout << RED ULINE"\nTEST WITH 20 BUT MAXIMUM IS 10"RESET << std::endl;
+	try {
+		s5.addNumber(5);
+		s5.addNumber(20);
+		s5.addNumber(23);
+		s5.addNumber(123);
+		s5.addNumber(32);
+		s5.addNumber(89);
+		s5.addNumber(123);
+		s5.addNumber(12);
+		s5.addNumber(1);
+		s5.addNumber(9084);
+		s5.addNumber(323);
+		s5.addNumber(23);
+		s5.addNumber(2123);
+		s5.addNumber(645);
+		s5.addNumber(65);
+		s5.addNumber(39);
+		s5.addNumber(34);
+		s5.addNumber(12);
+		s5.addNumber(997);
+		s5.addNumber(999);
+		s5.addNumber(998);
+		std::cout << "Inside Span: "YELLOW;
+		displayVector(s5);
+		std::cout << RESET;
+		std::cout << "Shortest length: "BLUE<< s5.shortestSpan() << RESET << std::endl;
+		std::cout << "Longest length: "MAGENTA << s5.longestSpan() << RESET << std::endl;
+	} catch (std::exception& e) {
+		std::cout << "Error: " << e.what() << RESET << std::endl;
+	}
+	Span s6 = Span(5);
+	std::cout << RED ULINE"\nTEST WITH 1 BUT MAXIMUM IS 5"RESET << std::endl;
+	try {
+		s6.addNumber(20);
+		std::cout << "Inside Span: "YELLOW;
+		displayVector(s6);
+		std::cout << RESET;
+		std::cout << s6.shortestSpan() << RESET << std::endl;
+		std::cout << s6.longestSpan() << RESET << std::endl;
+	} catch (std::exception& e) {
+		std::cout << "Error: " << e.what() << RESET << std::endl;
+	}
+	std::cout << RED ULINE"\nTEST WITH 4 MORE BUT MAXIMUM IS 5"RESET << std::endl;
+	try {
+		s6.addNumber(121);
+		s6.addNumber(89);
+		s6.addNumber(56);
+		s6.addNumber(34);
+		s6.addNumber(21);
+		std::cout << "Inside Span: "YELLOW;
+		displayVector(s6);
+		std::cout << RESET;
+		std::cout << "Shortest length: "BLUE<< s6.shortestSpan() << RESET << std::endl;
+		std::cout << "Longest length: "MAGENTA << s6.longestSpan() << RESET << std::endl;
 	} catch (std::exception& e) {
 		std::cout << "Error: " << e.what() << RESET << std::endl;
 	}
