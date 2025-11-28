de958e12b481d0f2a4623ee616671f74b65b5f79
maximart <maxime.martinez96@hotmail.fr>
2025-11-28 17:24:31 +0100

    chore: add some test and colors

--- cpp08/ex00/src/main.cpp
+++ cpp08/ex00/src/main.cpp
@@ -1,34 +1,68 @@
 #include "easyfind.hpp"
-
 #include <vector>
 #include <list>
 
+#define BLACK		"\033[1;30m"
+#define RED			"\033[0;31m"
+#define GREEN		"\033[1;32m"
+#define YELLOW		"\033[1;33m"
+#define BLUE		"\033[1;34m"
+#define MAGENTA		"\033[1;35m"
+#define CYAN		"\033[0;36m"
+#define WHITE		"\033[0;37m"
+#define RESET		"\033[0m"
+#define BOLD		"\033[1m"
+#define ULINE		"\033[4m"
+#define REVERSE		"\033[7m"
+
 template<typename T>
-void	testContainer(T& container, const int n) {
+void	pushEvenNumber(T& container, const int n) {
 	for (int i = 0; i < n; i++) {
 		if (i % 2 == 0)
 			container.push_back(i);
 	}
 }
 
-int main(int argc, char **argv) {
-	if (argc != 2)
-		return (1);
-	else {
-		try {
-			std::vector<int> vector;
-			testContainer(vector, 10);
-			easyfind(vector, std::atoi(argv[1]));
-		} catch (const std::exception& e) {
-			std::cout << e.what() << std::endl;
-		}
-		try {
-			std::list<int> list;
-			testContainer(list, 10);
-			easyfind(list, std::atoi(argv[1]));
-		} catch (const std::exception& e) {
-			std::cout << e.what() << std::endl;
-		}
+int main() {
+	std::cout << RED ULINE"\nTEST VECTOR FOUND ELEMENT"RESET << std::endl;
+	try {
+		std::vector<int> vector;
+		pushEvenNumber(vector, 10);
+		std::cout << YELLOW;
+		easyfind(vector, 2);
+		std::cout << RESET;
+	} catch (const std::exception& e) {
+		std::cout << e.what() << std::endl;
+	}
+	std::cout << RED ULINE"\nTEST VECTOR NOT FOUND ELEMENT"RESET << std::endl;
+	try {
+		std::vector<int> vector;
+		pushEvenNumber(vector, 10);
+		std::cout << BLUE;
+		easyfind(vector, 1);
+		std::cout << RESET;
+	} catch (const std::exception& e) {
+		std::cout << e.what() << std::endl;
+	}
+	std::cout << RED ULINE"\nTEST LIST FOUND ELEMENT"RESET << std::endl;
+	try {
+		std::list<int> list;
+		pushEvenNumber(list, 10);
+		std::cout << YELLOW;
+		easyfind(list, 8);
+		std::cout << RESET;
+	} catch (const std::exception& e) {
+		std::cout << e.what() << std::endl;
+	}
+	std::cout << RED ULINE"\nTEST LIST NOT FOUND ELEMENT"RESET << std::endl;
+	try {
+		std::list<int> list;
+		pushEvenNumber(list, 10);
+		std::cout << BLUE;
+		easyfind(list, 1);
+		std::cout << RESET;
+	} catch (const std::exception& e) {
+		std::cout << e.what() << std::endl;
 	}
 	return (0);
 }
