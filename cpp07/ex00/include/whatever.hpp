8693aef8bd81cd2f7619bb3591242047cc94435e
maximart <maxime.martinez96@hotmail.fr>
2025-11-28 13:15:11 +0100

    chore: addd some colors into the main

--- cpp07/ex00/include/whatever.hpp
+++ cpp07/ex00/include/whatever.hpp
@@ -2,19 +2,19 @@
 #define WHATEVER_HPP
 
 template<typename T>
-void swap(T &a, T &b){
+void swap(T& a, T& b){
 	T temp = a;
 	a = b;
 	b = temp;
 }
 
 template<typename U>
-U min(U a, U b) {
+U& min(U& a, U& b) {
 	return (a < b) ? a : b;
 }
 
 template<typename V>
-V max (V a, V b) {
+V& max (V& a, V& b) {
 	return (a > b) ? a : b;
 }
 
--- cpp07/ex00/src/main.cpp
+++ cpp07/ex00/src/main.cpp
@@ -1,22 +1,49 @@
 #include <iostream>
 #include "whatever.hpp"
 
+#define BLACK		"\033[1;30m"
+#define RED			"\033[0;31m"
+#define GREEN		"\033[1;32m"
+#define YELLOW		"\033[1;33m"
+#define BLUE		"\033[1;34m"
+#define MAGENTA		"\033[1;35m"
+#define CYAN		"\033[0;36m"
+#define WHITE		"\033[0;37m"
+#define RESET		"\033[0m"
+#define BOLD		"\033[1m"
+#define ULINE		"\033[4m"
+#define REVERSE		"\033[7m"
+
 int main( ) {
+	std::cout << RED ULINE BOLD"\nINT TEST"RESET << std::endl;
 	int a = 2;
 	int b = 3;
+	std::cout << "a = "BLUE << a << RESET", b = "MAGENTA << b << RESET << std::endl;
+	std::cout << YELLOW"SWAP"RESET << std::endl;
 	::swap( a, b );
-	std::cout << "a = " << a << ", b = " << b << std::endl;
+	std::cout << "a = "MAGENTA << a << RESET", b = "BLUE << b << RESET << std::endl;
+	std::cout << YELLOW"SWAP"RESET << std::endl;
 	::swap( a, b );
-	std::cout << "a = " << a << ", b = " << b << std::endl;
+	std::cout << "a = "BLUE << a << RESET", b = "MAGENTA << b << RESET << std::endl;
+	std::cout << YELLOW"SWAP"RESET << std::endl;
 	::swap( a, b );
-	std::cout << "a = " << a << ", b = " << b << std::endl;
-	std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
-	std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
+	std::cout << "a = "MAGENTA << a << RESET", b = "BLUE << b << RESET << std::endl;
+	std::cout << "min( a, b ) = "BLUE << ::min( a, b ) << RESET << std::endl;
+	std::cout << "max( a, b ) = "MAGENTA << ::max( a, b ) << RESET << std::endl;
+	std::cout << RED ULINE BOLD"\nSTRING TEST"RESET << std::endl;
 	std::string c = "chaine1";
 	std::string d = "chaine2";
+	std::cout << "c = "BLUE << c << RESET", d = "MAGENTA << d << RESET << std::endl;
+	std::cout << YELLOW"SWAP"RESET << std::endl;
 	::swap(c, d);
-	std::cout << "c = " << c << ", d = " << d << std::endl;
-	std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
-	std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
+	std::cout << "c = "MAGENTA << c << RESET", d = "BLUE << d << RESET << std::endl;
+	std::cout << YELLOW"SWAP"RESET << std::endl;
+	::swap(c, d);
+	std::cout << "c = "BLUE << c << RESET", d = "MAGENTA << d << RESET << std::endl;
+	std::cout << YELLOW"SWAP"RESET << std::endl;
+	::swap(c, d);
+	std::cout << "c = "MAGENTA << c << RESET", d = "BLUE << d << RESET << std::endl;
+	std::cout << "min( c, d ) = "BLUE << ::min( c, d ) << RESET << std::endl;
+	std::cout << "max( c, d ) = "MAGENTA << ::max( c, d ) << RESET << std::endl;
 	return 0;
 }
