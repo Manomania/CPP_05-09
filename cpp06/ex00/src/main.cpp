a9a4938b266de79cd03be46725731e48a13248ac
Manomania <34182052+Manomania@users.noreply.github.com>
2026-02-10 16:41:56 +0100

    Fix formatting in main.cpp

--- cpp06/ex00/src/main.cpp
+++ cpp06/ex00/src/main.cpp
@@ -2,7 +2,7 @@
 
 int main(int argc, char **argv) {
 	if (argc == 2) {
-		ScalarConverter::convert(argv[1]);
+		ScalarConverter::convert(argv[1]); 
 	} else {
 		std::cout << "\x1B[31mUsage: " << argv[0] << " <parameter1>\033[0m\t\t"<< std::endl;
 		return (1);
